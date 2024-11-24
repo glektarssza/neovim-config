9f2878c0d988e964a5d9b15f2b7c74bf768b3e80
G'lek Tarssza <glek@glektarssza.com>
2024-11-23 20:51:10 -0700

    Frameworked things out.

---
+++ lua/glek/init.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+
+require("glek.lazy_init")
+require("glek.remap")
+require("glek.set")
---
+++ lua/glek/set.lua
@@ -0,0 +1,24 @@
+-- Set color scheme
+-- vim.cmd.colorscheme("")
+
+-- Set system keyboard for yank
+vim.opt.clipboard = "unnamedplus"
+
+-- Use line numbers
+vim.opt.nu = true
+
+-- set tab size to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Disable wrapping
+vim.opt.wrap = false
+
+-- Enable incremental searching
+vim.opt.incsearch = true
+
+-- Enable terminal GUI colors
+vim.opt.termguicolors = true
