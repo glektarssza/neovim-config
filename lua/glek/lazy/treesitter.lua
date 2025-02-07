da952237666829a7da0e79bee51609cf238a5f2a
G'lek Tarssza <glek@glektarssza.com>
2025-02-07 13:35:42 -0700

    Corrected spelling of `:TSUpdate` command.

--- lua/glek/lazy/treesitter.lua
+++ lua/glek/lazy/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUdate",
+    build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
