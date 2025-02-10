5a0f5876bf9b90dc6f48667ca6aa2a96dd4c6c77
G'lek Tarssza <glek@glektarssza.com>
2025-02-10 11:43:12 -0700

    Format `lazy_init.lua`

--- lua/glek/lazy_init.lua
+++ lua/glek/lazy_init.lua
@@ -1,17 +1,17 @@
 local lazyPath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazyPath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazyPath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazyPath,
+    })
 end
 vim.opt.rtp:prepend(lazyPath)
 
 require("lazy").setup({
     spec = "glek.lazy",
-    change_detection = { notify = false }
+    change_detection = { notify = false },
 })
