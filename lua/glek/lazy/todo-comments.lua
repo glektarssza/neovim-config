19f83e23de60eda7f45af6fb47a5027d471d5b80
G'lek Tarssza <glek@glektarssza.com>
2025-02-15 22:06:48 -0700

    Added some more todo comment types.

--- lua/glek/lazy/todo-comments.lua
+++ lua/glek/lazy/todo-comments.lua
@@ -3,6 +3,67 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         require("todo-comments").setup({
+            keywords = {
+                FIX = {
+                    icon = " ",
+                    color = "error",
+                    alt = {
+                        "FIXME",
+                        "BUG",
+                        "FIXIT",
+                        "ISSUE",
+                        "!!",
+                    },
+                },
+                TODO = {
+                    icon = " ",
+                    color = "info",
+                },
+                HACK = {
+                    icon = " ",
+                    color = "warning",
+                },
+                WARN = {
+                    icon = " ",
+                    color = "warning",
+                    alt = {
+                        "WARNING",
+                        "XXX",
+                    },
+                },
+                PERF = {
+                    icon = " ",
+                    alt = {
+                        "OPTIM",
+                        "PERFORMANCE",
+                        "OPTIMIZE",
+                    },
+                },
+                NOTE = {
+                    icon = " ",
+                    color = "hint",
+                    alt = {
+                        "INFO",
+                        "--",
+                    },
+                },
+                TEST = {
+                    icon = "⏲ ",
+                    color = "test",
+                    alt = {
+                        "TESTING",
+                        "PASSED",
+                        "FAILED",
+                    },
+                },
+                QUESTION = {
+                    icon = "? ",
+                    color = "#3498DB",
+                    alt = {
+                        "??",
+                    },
+                },
+            },
             highlight = {
                 pattern = [[.*<(KEYWORDS)]],
             },
