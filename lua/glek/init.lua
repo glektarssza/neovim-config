4065e81ba5a9c6110f1f8d0f0691e233243b774c
G'lek Tarssza <glek@glektarssza.com>
2026-01-16 08:43:52 -0700

    Updated to latest configs.

--- lua/glek/init.lua
+++ lua/glek/init.lua
@@ -1,3 +1,5 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = "??"
 vim.g.markdown_fenced_languages = {
     "ts=typescript",
@@ -7,3 +9,4 @@ require("glek.lazy_init")
 require("glek.remap")
 require("glek.set")
 require("glek.cmds")
+require("glek.filetypes")
